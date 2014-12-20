28ae8550a5ec7f69cbef71fbf846eacb588f0d4a
Spencer Cohen <sc@moosoftware.com>
2014-12-19 19:59:57 -0800

    basic runtime impl

--- examples/underscore.js
+++ examples/underscore.js
@@ -1,11 +1,13 @@
-// Definition for an underscore wrapper property on all objects
+// Definition for an underscore wrapper property
+// i.e instead of _.pluck(foo, 'bar'), you can use foo._.pluck('bar')
+
 module.exports = function (cezve) {
   var _ = require('underscore');
 
-  cezve.addDefinition({
-    name: '_',
-    cache: true,
-    method: function (obj) {
+  cezve.addProperty({
+    name: '_', // name of the property
+    allowOverwrite: true, // client code can overwrite this property on a particular object
+    method: function (obj) { // getter
       return _(obj);
     },
   });
